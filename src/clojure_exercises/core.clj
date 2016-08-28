8f6d8cfdf26278e8021cf9f6a981fe1705c54430
russellpipal <russell.pipal@gmail.com>
2016-08-28 18:02:10 -0500

    exercise 17 completed

--- src/clojure_exercises/core.clj
+++ src/clojure_exercises/core.clj
@@ -15,3 +15,7 @@
 ;; #16 Write a function which returns a personalized greeting
 (= ((fn [x] (str "Hello, " x "!")) "Dave") "Hello, Dave!")
 ;;returns true. Function is (fn [x] (str "Hello, " x "!"))
+
+;; #17 The map function takes two arguments: a function (f) and a sequence (s). Map returns a new sequence consisting of the result of applying f to each item of s. Do not confuse the map function with the map data structure.
+(= '(6 7 8) (map #(+ % 5) '(1 2 3)))
+;;returns true. Maps the function (+ % 5) onto the list '(1 2 3)
