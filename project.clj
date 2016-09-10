1ff6fd82cc3413288ef73f8fed698ab59322774c
russellpipal <russell.pipal@gmail.com>
2016-09-10 18:48:38 -0500

    updates project.cli

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject clojure-exercises "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Clojure exercises"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
